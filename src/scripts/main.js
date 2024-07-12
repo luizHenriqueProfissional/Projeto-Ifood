fe99e53df8e024202d8586ba43def717100e0f66
luiz <luiz@gmail.com>
2024-07-11 22:48:53 -0300

    Iniciando projeto ifood

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 const abreCarrinho = document.getElementById('carrinho')
     abreCarrinho.addEventListener('click',function(){
         if(abreCarrinho){
-        const carrinhoDeCompras = document.getElementById('carrinho-de-compras')
-        carrinhoDeCompras.style.display = 'block'
+            setTimeout(function(){
+                const carrinhoDeCompras = document.getElementById('carrinho-de-compras')
+                carrinhoDeCompras.style.display = 'block'
+            },2000)
         }
         else{
-            const fecharPedidos = document.getElementById('fechar-pedidos')
-            fecharPedidos.style.display = 'none'
+            setTimeout(function(){
+                const fecharPedidos = document.getElementById('fechar-pedidos')
+                fecharPedidos.style.display = 'none'
+            },2000)
         }
     })
