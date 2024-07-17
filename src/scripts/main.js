df1b2c8b552aa5e1647137f23affc3b6975d1951
luizHenriqueProfissional <166259904+luizHenriqueProfissional@users.noreply.github.com>
2024-07-17 15:33:20 -0300

    Atualizar o main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,6 +21,6 @@ produto1 += `<span>R$30,00</span>`
 produto1 += '</li>'
 
 const conteudoLateral = document.getElementById(‘conteúdos ')
-conteudoLateral.innerHtml = produtos.value
+conteudoLateral.innerHtml = produtos
 alert(conteudoLateral)
 
