e86db4e5f45ea43d81891cb062e7393a302e837f
luizHenriqueProfissional <166259904+luizHenriqueProfissional@users.noreply.github.com>
2024-07-17 15:14:58 -0300

    Atualizar o main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,4 +11,15 @@ const abreCarrinho = document.getElementById('carrinho')
                 fecharPedidos.style.display = 'none'
         }
     })
-    alert('ola mundo')
+    let produtos
+produto1 = '<li>'
+produto1 += ’<img src="./imagens/carrosel/combo-hamburguer-acompanhamentos.jpg" alt="">’
+produto1 += ’<div class="description__laches">’
+produto1 += ’<h5>Combo Hamburguer</h5>’
+produto1 += ’<p>2 carnes, queijo, presunto + acompanhamentos</p>’
+produto1 += ’<span>R$30,00</span>’
+produto1 += '</li>'
+
+Const conteudoLateral = document.getElementById(‘conteúdos ')
+conteudoLateral.innerHtml = produtos
+alert(produtos)
